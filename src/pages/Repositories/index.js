000d17504141bd84ee1892713ac756507ff2cd60
Gisele Caetano Pereira <87086783+gicaetano@users.noreply.github.com>
2021-08-14 21:57:41 -0300

    :package:inclusão/mod pages

---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as S from './styled';
+
+export default function Repositories() {
+    return (
+        <S.Container>
+        <S.Title> Repositórios </S.Title>
+        <S.List>
+            <S.listItem> Repositório: Nome Repositório </S.listItem>
+            <S.listItem> Repositório: Nome Repositório </S.listItem>
+            <S.listItem> Repositório: Nome Repositório </S.listItem>
+            <S.listItem> Repositório: Nome Repositório </S.listItem>
+            <S.listItem> Repositório: Nome Repositório </S.listItem>
+            <S.listItem> Repositório: Nome Repositório </S.listItem>
+            <S.listItem> Repositório: Nome Repositório </S.listItem>
+            <S.listItem> Repositório: Nome Repositório </S.listItem>
+            <S.listItem> Repositório: Nome Repositório </S.listItem>
+            <S.listItem> Repositório: Nome Repositório </S.listItem>  
+        </S.List>
+        </S.Container>
+    )
+}
